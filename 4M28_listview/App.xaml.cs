aaacf2328417b90d494b78495c6c033ab1625b88
4m <4m@S213-14>
2024-04-11 08:27:22 +0200

    Dodaj pliki projektów.

---
+++ 4M28_listview/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace _4M28_listview
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ 4M28_listview/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace _4M28_listview
+{
+    public class Uczen{
+        public int nr {  get; set; }
+        public string imie {  get; set; }
+        public string klasa { get; set; }
+        public Uczen(int nr, string imie, string klasa){
+            this.nr = nr;
+            this.imie = imie;
+            this.klasa = klasa;
+        }
+        public override string ToString(){
+            return $"{nr} {imie} {klasa}";
+        }
+    }
+    public class Uczniowie{
+        public static List<Uczen> uczniowie = new List<Uczen>(){
+            new(1, "Ala", "Kot")
+        };
+    }
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+}
